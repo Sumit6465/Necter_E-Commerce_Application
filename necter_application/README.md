d03c04bcdf213e97a95fb631b625ba1f6dcc77f4
Sumit6465 <sumitkatkar6465@gmail.com>
2025-01-20 17:50:27 +0530

    WIP

---
+++ necter_application/README.md
@@ -0,0 +1,3 @@
+# necter_application
+
+A new Flutter project.
---
+++ necter_application/android/app/src/main/kotlin/com/example/necter_application/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.necter_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
